85cb101573ebddf16a96d47bbadc1c7415d4969f
Ujjwalb09 <ujjwalbhatt09@gmail.com>
2023-04-21 21:43:43 +0530

    Project Completed

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,24 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private boolean extraCheese = false;
+
+    private boolean extraToppings = false;
+
+    private boolean paperBag = false;
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+
+        if(isVeg = true)
+        {
+            this.price += 300;
+        }
+
+        else
+        {
+            this.price += 400;
+        }
     }
 
     public int getPrice(){
@@ -16,19 +31,78 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+
+        if(extraCheese) //adding check so that I don't add extraCheese amount again
+        {
+            return;
+        }
+        this.price += 80;
+        this.extraCheese = true;
     }
 
     public void addExtraToppings(){
-        // your code goes here
+
+        if(extraToppings)
+        {
+            return;
+        }
+
+        if(isVeg)
+        {
+            this.price += 70;
+        } else
+        {
+            this.price += 120;
+        }
+
+        this.extraToppings = true;
     }
 
     public void addTakeaway(){
-        // your code goes here
+
+        if(paperBag)
+        {
+            return;
+        }
+
+        this.price += 20;
+
+        this.paperBag = true;
     }
 
     public String getBill(){
-        // your code goes here
+
+        if(isVeg)
+        {
+            bill = "Base Price Of The Pizza: 300\n";
+        } else{
+            bill = "Base Price Of The Pizza: 400\n";
+        }
+
+        if(extraCheese)
+        {
+            bill += "Extra Cheese Added: 80\n";
+        }
+
+        if(extraToppings)
+        {
+            if(isVeg)
+            {
+                bill += "Extra Toppings Added: 70\n";
+            } else {
+                bill += "Extra Toppings Added: 120\n";
+            }
+        }
+
+        if(paperBag)
+        {
+            bill += "Paper Added: 20\n";
+        }
+
+        String totalPrice = Integer.toString(price);
+
+        bill += "Total Price: " + totalPrice + "\n";
+
         return this.bill;
     }
 }
